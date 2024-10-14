34be44786b34778e764eca995f91a3e48c3d6a29
Tatiana <tatyanakoroleva00@mail.ru>
2024-10-14 21:19:59 +0300

    button back and forward works well, there was a problem with states

--- src/main/InitialData.jsx
+++ src/main/InitialData.jsx
@@ -4,9 +4,6 @@ import Modal from '../modal_windows/Modal';
 
 const InitialData = ({ setInitialForm, initialForm }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
   const closeModal = () => {
     setIsModalOpen(false);
   };
@@ -17,12 +14,9 @@ const InitialData = ({ setInitialForm, initialForm }) => {
   return (
     <>
       {initialForm && <div>
-        <button onClick={openModal} >К списку проектов</button>
+        <button onClick={() => setIsModalOpen(true)} >К списку проектов</button>
         <Modal isOpen={isModalOpen} onClose={closeModal} onConfirm={confirmModal} header="Переход к списку проектов" message="Несохраненные данные могут быть потеряны. Вы готовы перейти?" answer1="Да" answer2="Нет, остаться" />
       </div>}
-      <div>
-        <button onClick={() => { setInitialForm(!initialForm); }}>{initialForm ? 'Вперед' : 'Назад'}</button>
-      </div>
     </>
   )
 }
--- src/main/StateTwo.jsx
+++ src/main/StateTwo.jsx
@@ -2,28 +2,46 @@ import React, { useState } from "react";
 import Interactives from "./Interactives";
 import styles from '../css/StateOne.module.css';
 import InitialDataForm from "./InitialDataForm";
-import InitialData from "./InitialData";
 import FinishCourse from "./FinishCourse";
+import Modal from '../modal_windows/Modal';
+
 const StateTwo = ({ globalData, serverDataGot, setGlobalData, interactives, setInteractives }) => {
   const [finishBtnClicked, setFinishBtnClicked] = useState(false);
   const [initialForm, setInitialForm] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const confirmModal = () => {
+    window.location.reload();
+  };
 
   return (
     <div className={`${finishBtnClicked && styles.hidden}`}>
+      {/* Заголовок проекта */}
       {globalData['heading'] && <h1 className={styles.title}>{globalData['heading']}</h1>}
+      
+      {/* Переход к списку проектов + модальное окно */}
+      {initialForm && <div>
+        <button onClick={() => setIsModalOpen(true)} >К списку проектов</button>
+        <Modal isOpen={isModalOpen} onClose={closeModal} onConfirm={confirmModal} header="Переход к списку проектов" message="Несохраненные данные могут быть потеряны. Вы готовы перейти?" answer1="Да" answer2="Нет, остаться" />
+      </div>}
 
+      {/* Кнопки "назад - вперед" и "завершить проект / сохранить изменения" */}
       <div className={styles['buttons-row']}>
-      <div><InitialData setInitialForm={setInitialForm} /></div>
+      <button onClick={() => setInitialForm(!initialForm)}>{initialForm ? 'Вперед' : 'Назад'}</button>
+      {!initialForm && <FinishCourse serverDataGot={serverDataGot} globalData={globalData} interactives={interactives} setFinishBtnClicked={setFinishBtnClicked} />
+      } </div>
 
-      <div><FinishCourse initialForm={initialForm} serverDataGot={serverDataGot} globalData={globalData} interactives={interactives} setFinishBtnClicked={setFinishBtnClicked} /></div>
-      </div>
-
+      {/* Первоначальная форма */}
       {initialForm && <InitialDataForm globalData={globalData} setGlobalData={setGlobalData} />}
 
-      <div>
+      {/* Интерактивы */}
+      {!initialForm && <div>
         <Interactives setInteractives={setInteractives} interactives={interactives} globalData={globalData} setGlobalData={setGlobalData} />
-      </div>
+      </div>}
 
+      {/* После завершения проекта - сообщение */}
       {finishBtnClicked && <div>
         <p>{serverDataGot ? 'Изменения сохранены' : 'Проект создан'}</p>
       </div>}
